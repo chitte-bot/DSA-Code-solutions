9aaef3d7f6a30f4d621dc7650c22ad052168f3dd
UMA1130 <chitteveena8@gmail.com>
2026-03-12 23:43:09 +0530

    Day - 8 Insert Node in Linked List
    Implemented a LinkedList with methods to insert nodes at the end and print the list.

---
+++ LinkedList/insert_node.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+
+
+list1 = LinkedList()
+
+list1.insert_end(10)
+list1.insert_end(20)
+list1.insert_end(30)
+list1.insert_end(40)
+
+list1.print_list()
