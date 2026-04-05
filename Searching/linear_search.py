8c5a01bf8d444bdfec49b26cd8bce9dca7737ee9
UMA1130 <chitteveena8@gmail.com>
2026-04-05 08:59:54 +0530

    Day 18 - Linear Search

---
+++ Searching/linear_search.py
@@ -0,0 +1,16 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+
+numbers = [10, 20, 30, 40]
+target = 30
+
+result = linear_search(numbers, target)
+
+if result != -1:
+    print("Element found at index", result)
+else:
+    print("Element not found")
